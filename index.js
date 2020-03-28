fc8d80df99055e6dbba0e022983eb6c1ffec1d30
johnnie1234 <55972949+johnnie1234@users.noreply.github.com>
2020-03-28 23:10:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,109 @@
+       const fruit = ['mango', 'kiwi'];
+       console.log(fruit.length);
+
+       let l = 2;
+      const m = 3;
+
+      console.log(l);
+    
+
+var hege = ["jan", "lul"];
+var stale = ["gek", "Tdada", "Lgag"];
+var children = hege.concat(stale);
+
+var str = "hoi";
+var res = str.replace("hoi", "Niemand");
+
+var str = "doei";
+var res = str.substring(1, 4);
+
+var str = "Dit is een toets";
+var res = str.split(" ");
+
+      console.log(l = m + 1);
+      console.log(l = l * m);
+      console.log(l = l % m);
+
+      console.log(l = l / m);
+
+      console.log(l = l - m);
+
+       alert('hoi');
+    
+       console.dir(document);
+       console.log(document.getElementById('main'));
+       const x = document.getElementById('niet main');
+       const z = document.getElementById('hoi');
+
+
+
+
+       function bodyy(){
+        document.body.style.backgroundColor = 'black';
+        alert(typeof '' === 'string');
+       }
+       function hoi (){
+           x.style.backgroundColor = 'blue';
+           document.body.style.backgroundImage = "url('iets.jpg')";
+            
+       }
+
+       function ja(){
+           alert(Math.floor(Math.random() * 10));
+       }
+
+
+       function iets(){
+        document.body.style.backgroundImage = "url('ietsanders.jpg')";
+        alert(1 + 1 * 2 / 3 - 1 % 2);
+       }
+
+ 
+    const items = document.getElementsByClassName('list-group-item');
+
+let num1 = 1;
+let num2 = 3;
+
+
+
+function rekenen(a=1, b=1) {
+  const total = a + b;
+  alert(total);
+}
+rekenen();
+
+    for(let i = 0; i < items.length; i++){
+            console.log('test' + i);
+            const kleuren = ['red','blue','green','yellow', 'purple', 'orange', 'pink' ];
+
+            
+        items[i].style.backgroundColor = kleuren[i];
+    items[i].textContent = 'hallo';
+    }
+
+
+
+    console.log(Math.random());
+
+    function color(){
+       const  i = math.random();
+    }
+const button = document.getElementById('nee');
+function hi(){
+    document.getElementById('header-title').style.backgroundColor = 'white';
+}
+
+function gekkeformat(){
+
+}
+
+ function buttonClick(){
+      console.log (' button is clicked');
+   document.getElementById('main-header').textContent = 'Hallo dit werkt';
+    document.querySelector('#main').style.backgroundColor = 'red';
+
+    function jo(){
+        document.getElementById('jo').style.backgroundColor = 'blue';
+    }
+
+ }
